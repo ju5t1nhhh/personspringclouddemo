232dec08761c4ebc6af1db677908c864530b11ca
Jackie2330489 <jiumam123@qq.com>
2017-10-20 16:44:13 +0800

    add dao-test

--- person/src/main/java/com/justin/person/domain/Person.java
+++ person/src/main/java/com/justin/person/domain/Person.java
@@ -8,7 +8,7 @@ import java.util.Date;
 /**
  * Created by Justin on 2017/10/20.
  */
-@Data
+//@Data
 public class Person implements Serializable{
     private Long id;
     private String name;
@@ -20,4 +20,84 @@ public class Person implements Serializable{
     private Integer status;
     private Date createTime;
     private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public Character getSex() {
+        return sex;
+    }
+
+    public void setSex(Character sex) {
+        this.sex = sex;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
 }
--- person/src/test/java/com/justin/person/dao/PersonDaoTest.java
+++ person/src/test/java/com/justin/person/dao/PersonDaoTest.java
@@ -25,29 +25,42 @@ public class PersonDaoTest {
 
     @Test
     public void insert() throws Exception {
+        Person person=new Person();
+        person.setName("Cat");
+        person.setPwd("666");
+        personDao.insert(person);
     }
 
     @Test
     public void deleteByPrimaryKey() throws Exception {
+        personDao.deleteByPrimaryKey(2L);
     }
 
     @Test
     public void updateByPrimaryKey() throws Exception {
+        Person person=new Person();
+        person.setId(1000L);
+        person.setName("Hahaha");
+        person.setPwd("abc");
+        personDao.updateByPrimaryKey(person);
     }
 
     @Test
     public void ifExists() throws Exception {
-        System.out.println(personDao.ifExists(1L));
+        personDao.ifExists("Cat","666");
     }
 
     @Test
     public void loginSelectList() throws Exception {
-        System.out.println(personDao.loginSelectList());
+        List<Person> people=personDao.loginSelectList();
+        for(Person person:people){
+            System.out.println(person);
+        }
     }
 
     @Test
     public void visitorSelectList() throws Exception {
-        List<Person> people=personDao.loginSelectList();
+        List<Person> people=personDao.visitorSelectList();
         for(Person person:people){
             System.out.println(person);
         }
